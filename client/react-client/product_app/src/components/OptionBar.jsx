d0595c67ea861edc97fb2b7c4a6c91f46d34856a
daviddbyrd <davbywork@gmail.com>
2025-04-13 23:34:16 +0100

    Added styling to Product component in React client.

--- client/react-client/product_app/src/components/OptionBar.jsx
+++ client/react-client/product_app/src/components/OptionBar.jsx
@@ -19,10 +19,15 @@ const OptionBar = ({ handleChangeView, viewBy, createNewProduct }) => {
     setForm(defaultForm);
   };
 
+  const onCreate = () => {
+    setIsCreating(false);
+    createNewProduct({ newProduct: form });
+  };
+
   return (
     <>
       <div className="flex flex-row justify-around w-6/10 mb-5">
-        <div className="w-4/10 text-center border-2 border-solid rounded-md">
+        <div className="w-4/10 text-center border-2 border-solid rounded-md flex items-center justify-center">
           <select
             value={viewBy}
             onChange={(e) => handleChangeView(e.target.value)}
@@ -33,54 +38,66 @@ const OptionBar = ({ handleChangeView, viewBy, createNewProduct }) => {
           </select>
         </div>
         <button
-          className="w-4/10 text-center border-2 border-solid rounded-md"
+          className="bg-green-200 py-1 px-5 border-2 border-black w-4/10 text-center border-2 border-solid rounded-md"
           onClick={() => setIsCreating(true)}
         >
-          Create New Product
+          Create New Product +
         </button>
       </div>
       {isCreating && (
-        <div className="bg-blue-100 p-4 rounded-md w-6/10">
-          <div className="w-5/10 h-10 flex items-center gap-2 px-4">
+        <div className="bg-blue-100 p-4 rounded-md w-6/10 my-10 border-2 border-black">
+          <div className="h-10 flex items-center gap-2 px-4">
             <h1>Name:</h1>
             <input
-              className="bg-white rounded-md px-2"
+              className="w-full bg-white rounded-md px-2"
               name="name"
               value={form.name}
               onChange={handleChange}
             />
           </div>
-          <div className="w-5/10 h-10 flex items-center gap-2 px-4">
+          <div className="h-10 flex items-center gap-2 px-4">
             <h1>Description:</h1>
             <input
-              className="bg-white rounded-md px-2"
+              className="w-full bg-white rounded-md px-2"
               name="description"
               value={form.description}
               onChange={handleChange}
             />
           </div>
-          <div className="w-5/10 h-10 flex items-center gap-2 px-4">
+          <div className="h-10 flex items-center gap-2 px-4">
             <h1>Price:</h1>
             <input
-              className="bg-white rounded-md px-2"
+              className="w-full bg-white rounded-md px-2"
               name="price"
               value={form.price}
               onChange={handleChange}
             />
           </div>
-          <div className="w-5/10 h-10 flex items-center gap-2 px-4">
-            <h1>Available:</h1>
-            <input
-              className="bg-white rounded-md px-2"
+          <div className="h-10 flex items-center gap-2 px-4">
+            <select
+              className="bg-white w-full rounded-md px-2"
               name="available"
               value={form.available}
               onChange={handleChange}
-            />
+            >
+              <option value="true">Available</option>
+              <option value="false">Unavailable</option>
+            </select>
           </div>
-          <button onClick={onCancel}>Chancel</button>
-          <button onClick={() => createNewProduct({ newProduct: form })}>
-            CreateProduct
-          </button>
+          <div className="w-full flex flex-row justify-around">
+            <button
+              className="bg-red-200 py-1 px-5 border-2 border-black rounded-md"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-green-200 py-1 px-5 border-2 border-black rounded-md"
+              onClick={() => onCreate()}
+            >
+              Create Product
+            </button>
+          </div>
         </div>
       )}
     </>
--- client/react-client/product_app/src/components/Product.jsx
+++ client/react-client/product_app/src/components/Product.jsx
@@ -27,35 +27,90 @@ const Product = ({
   }
 
   return isEditing ? (
-    <div className="bg-blue-100 p-4 rounded-md w-6/10">
-      <input name="name" value={form.name} onChange={handleChange} />
-      <input
-        name="description"
-        value={form.description}
-        onChange={handleChange}
-      />
-      <input name="price" value={form.price} onChange={handleChange} />
-      <input name="available" value={form.available} onChange={handleChange} />
-      <button onClick={onCancel}>Chancel</button>
-      <button onClick={() => onSaveEdit({ id, updatedProduct: form })}>
-        Save Changes
-      </button>
+    <div className="bg-blue-100 p-4 border-2 border-black rounded-md">
+      <div className="h-10 flex items-center gap-2 px-4">
+        <h1>Name:</h1>
+        <input
+          className="bg-white w-full rounded-md px-2"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="h-10 flex items-center gap-2 px-4">
+        <h1>Description:</h1>
+        <input
+          className="bg-white w-full rounded-md px-2"
+          name="description"
+          value={form.description}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="h-10 flex items-center gap-2 px-4">
+        <h1>Price:</h1>
+        <input
+          className="bg-white w-full rounded-md px-2"
+          name="price"
+          value={form.price}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="h-10 flex items-center gap-2 px-4">
+        <h1>Available:</h1>
+        <select
+          className="bg-white w-full rounded-md px-2"
+          name="available"
+          value={form.available}
+          onChange={handleChange}
+        >
+          <option value="true">Available</option>
+          <option value="false">Unavailable</option>
+        </select>
+      </div>
+      <div className="w-full flex flex-row justify-around">
+        <button
+          className="bg-red-200 py-1 px-5 border-2 border-black rounded-md"
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-green-200 py-1 px-5 border-2 border-black rounded-md"
+          onClick={() => onSaveEdit({ id, newProduct: form })}
+        >
+          CreateProduct
+        </button>
+      </div>
     </div>
   ) : (
-    <div className="bg-blue-100 p-4 rounded-md">
-      <h1>{id}</h1>
-      <h1>{name}</h1>
-      <h1>{description}</h1>
-      <h1>€{price}</h1>
-      <h1>{available}</h1>
-      <div>
+    <div className="bg-blue-100 p-4 border-2 border-black rounded-md">
+      <div className="w-full h-10 flex items-center gap-2 px-4">
+        <h1>Name:</h1>
+        <span className="px-2">{name}</span>
+      </div>
+      <div className="w-full h-10 flex items-center gap-2 px-4">
+        <h1>Description:</h1>
+        <span className="px-2">{description}</span>
+      </div>
+      <div></div>
+      <div className="w-full h-10 flex items-center gap-2 px-4">
+        <h1>Price:</h1>
+        <span className="px-2">€{price}</span>
+      </div>
+      <div className="w-full h-10 flex items-center gap-2 px-4">
+        {available ? <span>Available</span> : <span>Unavailable</span>}
+      </div>
+      <div className="w-full flex flex-row justify-around">
         <button
-          className="border-2 border-solid"
+          className="bg-red-200 py-1 px-5 border-2 border-black rounded-md"
           onClick={() => deleteProduct(id)}
         >
           Delete
         </button>
-        <button className="border-2 border-solid" onClick={onEdit}>
+        <button
+          className="bg-yellow-200 py-1 px-5 border-2 border-black rounded-md"
+          onClick={onEdit}
+        >
           Edit
         </button>
       </div>
