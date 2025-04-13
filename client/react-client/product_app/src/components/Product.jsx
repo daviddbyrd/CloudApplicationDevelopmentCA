ae921568ef487ddd55d3f4651ccffbc1fd047adc
daviddbyrd <davbywork@gmail.com>
2025-04-13 02:12:55 +0100

    Added tailwind to React Client

--- client/react-client/product_app/src/components/Product.jsx
+++ client/react-client/product_app/src/components/Product.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Product = ({ name, description, price, available }) => {
   return (
-    <>
+    <div>
       <h1>{name}</h1>
       <h1>{description}</h1>
       <h1>{price}</h1>
       <h1>{available}</h1>
-    </>
+    </div>
   );
 };
 
