cffe4b550df87e6c48996cb8713b66968e74667a
daviddbyrd <davbywork@gmail.com>
2025-04-15 18:03:21 +0100

    Added 'Create Product' functionality to HTML client with HTTP request to Ruby on Rails Server.

--- client/html-client/app.js
+++ client/html-client/app.js
@@ -2,6 +2,48 @@ const render = () => {
   const productContainer = document.getElementById("products");
 
   productContainer.innerHTML = "";
+  if (isCreating) {
+    const newProduct = {
+      name: "",
+      description: "",
+      price: 0,
+      available: true,
+    };
+
+    const creatingDiv = document.createElement("div");
+
+    creatingDiv.innerHTML = `
+        <b>Name: </b> <input value="${newProduct.name}" data-key="name"> <br/>
+        <b>Description: </b> <input value="${newProduct.description}" data-key="description"> <br/>
+        <b>Price: </b> <input value="${newProduct.price}" data-key="price"> <br/>
+        <b>Available: </b> <input value="${newProduct.available}" data-key="available"> <br/>
+        <button>Save Changes</button>
+        <button>Cancel</button>
+        <br/>
+    `;
+
+    const inputs = creatingDiv.querySelectorAll("input");
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", (e) => {
+        newProduct[input.dataset.key] = e.target.value;
+      });
+    });
+
+    const buttons = creatingDiv.querySelectorAll("button");
+    buttons[0].addEventListener("click", () => createProduct(newProduct));
+    buttons[1].addEventListener("click", () => cancelCreateProduct());
+
+    productContainer.appendChild(creatingDiv);
+  } else {
+    const creatingDiv = document.createElement("div");
+    creatingDiv.innerHTML = `
+        <button>Create Product</button>
+    `;
+    const button = creatingDiv.querySelector("button");
+    button.addEventListener("click", () => startCreateProduct());
+    productContainer.appendChild(creatingDiv);
+  }
 
   data.forEach((product) => {
     const productDiv = document.createElement("div");
@@ -14,6 +56,7 @@ const render = () => {
         <b>Available: </b> <input value="${product.temp.available}" data-key="available"> <br/>
         <button>Save Changes</button>
         <button>Cancel</button>
+        <br/>
       `;
 
       const inputs = productDiv.querySelectorAll("input");
@@ -35,6 +78,7 @@ const render = () => {
         <b>Available: </b> ${product.available} <br/>
         <button>Edit Product</button>
         <button>Delete Product</button>
+        <br/>
       `;
 
       const buttons = productDiv.querySelectorAll("button");
@@ -58,6 +102,29 @@ const loadData = async () => {
     product["temp"] = {};
   });
   render();
+};
+
+const startCreateProduct = () => {
+  isCreating = true;
+  render();
+};
+
+const cancelCreateProduct = () => {
+  isCreating = false;
+  render();
+};
+
+const createProduct = async (newProduct) => {
+  var url = `http://localhost:3000/products`;
+  var response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newProduct),
+  });
+  loadData();
 };
 
 const editProduct = (id) => {
@@ -116,5 +183,6 @@ const deleteProduct = async (id) => {
 };
 
 var data = [];
+var isCreating = false;
 
 window.addEventListener("DOMContentLoaded", loadData);
