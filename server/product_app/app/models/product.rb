a7aa1e156614330d3c76124437c009df4b24611c
daviddbyrd <davbywork@gmail.com>
2025-04-27 12:31:57 +0100

    Added more Product models tests to Rails.

--- server/product_app/app/models/product.rb
+++ server/product_app/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :available, inclusion: { in: [true, false] }
 end
--- server/product_app/spec/models/product_spec.rb
+++ server/product_app/spec/models/product_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Product, type: :model do
 end
 
 RSpec.describe Product, type: :model do
-  it "is invalid when missing name" do
+  it "is invalid without name" do
     product = Product.new(
       description: "A Car",
       price: "100",
@@ -22,3 +22,46 @@ RSpec.describe Product, type: :model do
     expect(product).not_to be_valid
   end
 end
+
+RSpec.describe Product, type: :model do
+  it "is invalid without description" do
+    product = Product.new(
+      name: "Car",
+      price: "100",
+      available: true
+    )
+    expect(product).not_to be_valid
+  end
+end
+
+RSpec.describe Product, type: :model do
+  it "is invalid without price" do
+    product = Product.new(
+      name: "Car",
+      description: "A Car",
+      available: true
+    )
+    expect(product).not_to be_valid
+  end
+end
+
+RSpec.describe Product, type: :model do
+  it "is invalid without available attribute" do
+    product = Product.new(
+      name: "Car",
+      description: "A Car",
+      price: 100
+    )
+    expect(product).not_to be_valid
+  end
+end
+
+RSpec describe Product, type: :model do
+  it "is invalid if it had additional attributes" do
+    product = Product.new(
+      name: "Car",
+      description: "A Car",
+      price: 100,
+      available: true
+      location: "Dublin"
+    )
