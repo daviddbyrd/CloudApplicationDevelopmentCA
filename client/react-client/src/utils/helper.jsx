b404fa63c8f847c72a133644ff6f6c007f918ec3
daviddbyrd <davbywork@gmail.com>
2025-04-27 23:56:54 +0100

    Changed data validation in React.

--- client/react-client/src/utils/helper.jsx
+++ client/react-client/src/utils/helper.jsx
@@ -1,25 +1,13 @@
 export const ValidateProductData = ({ product }) => {
-  const validKeys = {
-    name: "string",
-    description: "string",
-    available: "boolean",
-  };
+  const validKeys = ["name", "description", "price", "available"];
 
   const productKeys = Object.keys(product);
   if (productKeys.length !== 4) {
-    console.log(`1 ${productKeys}`);
     return false;
   }
 
-  if (!("price" in product)) {
-    return false;
-  }
-
-  // Need to insert Validation for Price to check if it is a number.
-
-  for (const [key, type] of Object.entries(validKeys)) {
-    if (!(key in product) || typeof product[key] !== type) {
-      console.log(`2 ${productKeys}, ${key}`);
+  for (const key of validKeys) {
+    if (!(key in product)) {
       return false;
     }
   }
